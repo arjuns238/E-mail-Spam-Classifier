57fb5b1c6bf71326385c218ff3dbc2293968f134
arjuns238 <92126319+arjuns238@users.noreply.github.com>
2024-01-14 14:13:08 -0500

    Update ex6.m

--- ex6.m
+++ ex6.m
@@ -1,21 +1,3 @@
-%% Machine Learning Online Class
-%  Exercise 6 | Support Vector Machines
-%
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  exercise. You will need to complete the following functions:
-%
-%     gaussianKernel.m
-%     dataset3Params.m
-%     processEmail.m
-%     emailFeatures.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
 %% Initialization
 clear ; close all; clc
 
